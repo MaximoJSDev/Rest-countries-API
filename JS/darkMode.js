0fec6b234bcb331ba545650984d1924bfdb9f9c0
Maximo-4ever <maximilianocm06@gmail.com>
2021-12-27 19:01:30 -0600

    agrega el color de preferencia del usuario

--- JS/darkMode.js
+++ JS/darkMode.js
@@ -1,4 +1,18 @@
 const html = document.documentElement;
 const btnDarkMode = document.querySelector(".navbar__light-mode");
+let dark = false;
 
-btnDarkMode.addEventListener("click", () => html.classList.toggle("dark-mode"));
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  dark = true;
+}
+btnDarkMode.addEventListener("click", () => {
+  if (dark) {
+    html.classList.remove("dark-mode");
+    html.classList.add("light-mode");
+    dark = false;
+  } else {
+    html.classList.remove("light-mode");
+    html.classList.add("dark-mode");
+    dark = true;
+  }
+});
